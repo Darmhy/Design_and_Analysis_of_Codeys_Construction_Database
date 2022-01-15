9ab9973b68d044426ce3f191a4dcb7ddae637255
Damilola Oyebade <40070230+Darmhy@users.noreply.github.com>
2022-01-15 13:39:14 +0000

    Update 1_buildschema_tables.sql

--- 1_design_and_build_database/1_buildschema_tables.sql
+++ 1_design_and_build_database/1_buildschema_tables.sql
@@ -1,38 +1,40 @@
+-- To build the database
+
 CREATE TABLE projects (
-    id     		INTEGER NOT NULL,
-    name      	VARCHAR(300) NOT NULL,
+    	id     		INTEGER NOT NULL,
+    	name      	VARCHAR(300) NOT NULL,
 	value     	FLOAT(8, 1),
-    start_date 	DATE NOT NULL,
-    end_date    DATE NOT NULL,
+    	start_date 	DATE NOT NULL,
+    	end_date    	DATE NOT NULL,
 	PRIMARY KEY("id" AUTOINCREMENT)
   );
 
 CREATE TABLE contractors (
-    id              INTEGER NOT NULL,
-    first_name      VARCHAR(300) NOT NULL,
-    last_name       VARCHAR(300) NOT NULL,
+    	id              INTEGER NOT NULL,
+    	first_name      VARCHAR(300) NOT NULL,
+    	last_name       VARCHAR(300) NOT NULL,
 	hourly_wage     FLOAT(5, 2) NOT NULL,
-    hire_date       DATE NOT NULL,
+    	hire_date       DATE NOT NULL,
 	PRIMARY KEY("id" AUTOINCREMENT)
   );
  
  
 CREATE TABLE employees (
-    id              INTEGER NOT NULL,
-    first_name      VARCHAR(300) NOT NULL,
-    last_name       VARCHAR(300) NOT NULL,
+    	id              INTEGER NOT NULL,
+    	first_name      VARCHAR(300) NOT NULL,
+    	last_name       VARCHAR(300) NOT NULL,
 	hourly_wage     FLOAT(5, 2) NOT NULL,
-    hire_date       DATE NOT NULL,
+    	hire_date       DATE NOT NULL,
 	PRIMARY KEY("id" AUTOINCREMENT)
   );
   
   
 CREATE TABLE job_orders (
-	id				INTEGER NOT NULL,
+	id			INTEGER NOT NULL,
 	employee_id		INTEGER NOT NULL, 
 	project_id		INTEGER NOT NULL, 
 	description		VARCHAR(300) NOT NULL,
-	order_datetime	DATETIME DEFAULT CURRENT_TIMESTAMP,
+	order_datetime		DATETIME DEFAULT CURRENT_TIMESTAMP,
 	quantity		INTEGER NOT NULL,
 	price			FLOAT(5, 2) NOT NULL,
 	PRIMARY KEY("id" AUTOINCREMENT),
@@ -49,23 +51,23 @@ CREATE TABLE project_employees (
   );
   
 CREATE TABLE project_feedbacks (
-	project_id			INTEGER NOT NULL,
-	customer_id			INTEGER NOT NULL,
+	project_id		INTEGER NOT NULL,
+	customer_id		INTEGER NOT NULL,
 	project_feedback	TEXT,
 	PRIMARY KEY("project_id", "customer_id")
   );
   
 CREATE TABLE customers (
-    name				VARCHAR(255),
-	industry			VARCHAR(255),
-	project1_id			INTEGER,
+    	name			VARCHAR(255),
+	industry		VARCHAR(255),
+	project1_id		INTEGER,
 	project1_feedback	TEXT,
-	project2_id			INTEGER,
+	project2_id		INTEGER,
 	project2_feedback	TEXT,
 	contact_person_id	INTEGER,
 	contact_person_and_role	VARCHAR(300),
 	phone_number		VARCHAR(12),
-	address				VARCHAR(255),
-	city				VARCHAR(255),
-	zip					VARCHAR(5)
+	address			VARCHAR(255),
+	city			VARCHAR(255),
+	zip			VARCHAR(5)
 	);
